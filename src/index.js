1f458fd0a99facbd8af853aeda43ce5f4e3dfaf6
Godwin <agblezeg@gmail.com>
2022-11-08 17:02:37 +0100

    Import modules and add changing page logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("Testing Testing....");
+import './style.css';
+import header from './modules/header.js';
+import home from './modules/homepage.js';
+import contact from './modules/contact.js';
+
+header();
+home();
+// contact();
+
+const homeBtn = document.querySelector("li.home");
+const menuBtn = document.querySelector("li.menu");
+const contactsBtn = document.querySelector("li.contacts");
+
+homeBtn.addEventListener("click", () => {
+  document.querySelector("#content").innerHTML = "";
+  home();
+});
+
+menuBtn.addEventListener("click", () => {
+  document.querySelector("#content").innerHTML = "";
+  /* Add menu code here */
+});
+
+contactsBtn.addEventListener("click", () => {
+  document.querySelector("#content").innerHTML = "";
+  contact();
+});
